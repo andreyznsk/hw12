08b54654323085b573d60d88327e51409ea5fa1c
andreyz <andreyz@gmail.com>
2024-12-10 09:24:42 +0700

    done

--- src/main/java/org/example/HomeWork.java
+++ src/main/java/org/example/HomeWork.java
@@ -3,8 +3,16 @@ package org.example;
 
 import lombok.SneakyThrows;
 
+import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class HomeWork {
 
@@ -14,9 +22,36 @@ public class HomeWork {
      */
     @SneakyThrows
     public void frogSteps(InputStream in, OutputStream out) {
-        //Тут будет код
+        List<String> frogList = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8))
+                .lines()
+                .collect(Collectors.toList());
+
+        int steps = Integer.parseInt(frogList.get(0));
+
+        System.out.println(frogList);
+
+        List<Integer> res = new ArrayList<>(steps);
+
+        for (int i = 1; i <= steps; i++) {
+            res.add(findMax(frogList.get(i).toCharArray()));
+
+        }
+
+        String resStr = res.stream().map(String::valueOf).collect(Collectors.joining(System.lineSeparator())) + System.lineSeparator();
+        out.write(resStr.getBytes(StandardCharsets.UTF_8));
     }
 
+    private Integer findMax(char[] charArray) {
+        int maxStep = 0;
+        int currentStep = 0;
+        for (char c : charArray) {
+            if (c == 'L') {
+                currentStep++;
+                maxStep = Math.max(maxStep, currentStep);
+            } else currentStep = 0;
+        }
+        return maxStep + 1;
+    }
 
 
 }
